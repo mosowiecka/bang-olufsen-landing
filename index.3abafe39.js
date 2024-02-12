01303cc574f5f7d62cea0e305f965e025190814e
Monika Osowiecka <mosowiecka@users.noreply.github.com>
2024-02-12 19:11:13 +0100

    make build

---
+++ index.3abafe39.js
@@ -0,0 +1,2 @@
+var e=document.getElementById("form");e.addEventListener("submit",function(t){t.preventDefault(),e.reset()}),document.getElementById("openLink").addEventListener("click",function(e){e.preventDefault();var t=this.getAttribute("href");window.open(t,"_blank")});
+//# sourceMappingURL=index.3abafe39.js.map
